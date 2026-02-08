1c8c4756af44163778407bf1d251d67687bf27b8
George Gil <georgegil3902@gmail.com>
2026-02-08 14:28:28 +0100

    removed unused variable name in definetion for Linux compatibility

--- src/FrameKit/Core/Engine/IAppHost.h
+++ src/FrameKit/Core/Engine/IAppHost.h
@@ -26,8 +26,8 @@ namespace FrameKit {
         virtual ~IAppHost() = default;
 		virtual bool Init(ApplicationBase& app) = 0;    // calls app.Init(); false => exit
 		virtual bool Tick(ApplicationBase& app) = 0;    // one iteration of run loop; false => exit
-		virtual bool StartAsyncWorker(ApplicationBase& app) { return true; } // optional async thread
-        virtual void StopAsyncWorker(ApplicationBase& app) {}
+		virtual bool StartAsyncWorker(ApplicationBase&) { return true; } // optional async thread
+        virtual void StopAsyncWorker(ApplicationBase&) {}
         virtual void SignalClose() = 0;
         virtual HostStats Stats() const = 0;
     };
